649944871a9ddba8834ab70bc05189d69421d1e7
Marcel <marcelfcordeiro@gmail.com>
2021-06-23 11:45:29 -0300

    finishing tests on auth.service

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -3,23 +3,73 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import * as ormconfig from '../../config/ormconfig';
 import { AuthModule } from '../auth.module';
-import { UserModule } from '../../user/user.module';
-import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../../user/services/user.service';
+import {
+  runInTransaction,
+  initialiseTestTransactions,
+} from 'typeorm-test-transactions';
+import { IUserLogin } from '../interfaces/user-login.interface';
+import { UserRole } from '../../user/entities/user.entity';
+
+initialiseTestTransactions();
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot(ormconfig)],
-      providers: [AuthService, JwtService, UserService],
+      imports: [TypeOrmModule.forRoot(ormconfig), AuthModule],
+      providers: [],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it(
+    'should be able to login a new User',
+    runInTransaction(async () => {
+      const userLogin: IUserLogin = {
+        email: 'user@user.com',
+        password: '12345678',
+      };
+
+      const result = await service.login(userLogin);
+
+      expect(result).toEqual({
+        user: {
+          role: UserRole.BASIC,
+          email: 'user@user.com',
+          id: expect.any(String),
+        },
+        token: expect.any(String),
+      });
+    }),
+  );
+
+  it(
+    'not should be able to login with invalid e-mail',
+    runInTransaction(async () => {
+      const userLogin: IUserLogin = {
+        email: 'user-invalid@user.com',
+        password: '12345678',
+      };
+
+      return expect(service.login(userLogin)).rejects.toThrow(
+        'Invalid credentials',
+      );
+    }),
+  );
+
+  it(
+    'not should be able to login with invalid password',
+    runInTransaction(async () => {
+      const userLogin: IUserLogin = {
+        email: 'user@user.com',
+        password: 'invalid-password',
+      };
+
+      return expect(service.login(userLogin)).rejects.toThrow(
+        'Invalid credentials',
+      );
+    }),
+  );
 });
