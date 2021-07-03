97f0332cc60720ec46c0408cc55a8b504aab03a8
Marcel <marcelfcordeiro@gmail.com>
2021-07-03 19:13:49 -0300

    add config database

--- database/migration.ormconfig.ts
+++ database/migration.ormconfig.ts
@@ -1,6 +1,8 @@
 import { ConnectionOptions } from 'typeorm';
 import * as ormconfig from '../src/config/ormconfig';
 
+console.log(process.env.DB_HOST);
+
 const config: ConnectionOptions = {
   ...ormconfig,
   logging: true,
