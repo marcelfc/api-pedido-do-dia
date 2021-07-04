3842009266c8d386c5e820c53417a158e1e5ee36
Marcel <marcelfcordeiro@gmail.com>
2021-07-04 10:07:56 -0300

    add job lint

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -5,7 +5,10 @@ const config: ConnectionOptions = {
   name: 'default',
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: process.env.STAGE === 'test' ? parseInt(process.env.POSTGRES_PORT_TEST) : parseInt(process.env.POSTGRES_PORT),
+  port:
+    process.env.STAGE === 'test'
+      ? parseInt(process.env.POSTGRES_PORT_TEST)
+      : parseInt(process.env.POSTGRES_PORT),
   database: process.env.POSTGRES_DB,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
